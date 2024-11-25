1b08a773e7d318b8394b6b1dc271fb74719ce7a6
yograj17 <yograjpatil777@gmail.com>
2024-11-25 12:44:20 +0530

    ok

--- router/router.js
+++ router/router.js
@@ -2,6 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const ResiterController = require("../controller/resiterController");
+const data = [
+  {
+    name: "one",
+    num: "1",
+  },
+  {
+    name: "two",
+    num: "2",
+  },
+  {
+    name: "three",
+    num: "3",
+  },
+  {
+    name: "four",
+    num: "4",
+  },
+];
+
+router.get("/api/data", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: data,
+  });
+});
 
 router.post("/api/regiter", ResiterController);
 
